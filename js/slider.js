e9e04f3763c11ef80da22e691b3792ff456d3a53
xeligio2029-cmyk <xeligio2029@gmail.com>
2025-12-29 00:33:30 -0500

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,10 @@
+let slides = document.querySelectorAll(".slide");
+let indice = 0;
 
+function mostrarSlide() {
+  slides.forEach(slide => slide.classList.remove("activo"));
+  slides[indice].classList.add("activo");
+  indice = (indice + 1) % slides.length;
+}
+
+setInterval(mostrarSlide, 3000);
